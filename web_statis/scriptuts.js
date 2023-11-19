0864d1773767742b94c9a4dc3296aca47b1885d3
Dewi Handayani <144642635+dewihandayanii@users.noreply.github.com>
2023-11-19 12:55:42 +0700

    Add files via upload

---
+++ web_statis/scriptuts.js
@@ -0,0 +1,38 @@
+alert("Selamat Datang Di Situs Timnas Senior Indonesia");
+function scrollToElement(elementSelector, instance = 0) {
+  // Select all elements that match the given selector
+  const elements = document.querySelectorAll(elementSelector);
+  // Check if there are elements matching the selector and if the requested instance exists
+  if (elements.length > instance) {
+    // Scroll to the specified instance of the element
+    elements[instance].scrollIntoView({ behavior: "smooth" });
+  }
+}
+
+const link1 = document.getElementById("link1");
+const link2 = document.getElementById("link2");
+const link3 = document.getElementById("link3");
+const link4 = document.getElementById("link4");
+
+link1.addEventListener("click", () => {
+  scrollToElement(".header");
+});
+
+link2.addEventListener("click", () => {
+  // Scroll to the second element with "header" class
+  scrollToElement(".header", 1);
+});
+
+link3.addEventListener("click", () => {
+  scrollToElement(".column");
+});
+
+link4.addEventListener("click", () => {
+  scrollToElement(".header", 2);
+});
+
+// Fungsi untuk menangani tampilan sidebar
+function toggleSidebar() {
+  var sidebar = document.querySelector('.sidenav');
+  sidebar.style.width = (sidebar.style.width === '250px') ? '0':'250px';
+}
